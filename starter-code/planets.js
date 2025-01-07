00b3f6d4005d694bf1598a621262a4034e4888de
Pelicie <mecagoenlashormigas@gmail.com>
2025-01-07 14:02:34 -0500

    Agregando los elementos necesarios del DOM para la implementacion del Json

--- starter-code/planets.js
+++ starter-code/planets.js
@@ -1,59 +1,11 @@
-const linksContainer = document.getElementById("links_container");
+const doc = document
+const planetsImage = doc.getElementById('planetsImage') 
+const moon = doc.getElementById('moon')
+const mars = doc.getElementById('mars')
+const europa = doc.getElementById('europa')
+const titan = doc.getElementById('titan')
+const title = doc.getElementById('title')
+const text = doc.getElementById('text')
+const distance = doc.getElementById('distance')
+const time = doc.getElementById('time')
 
-const planetsLinks = [
-  {
-    text: "Moon",
-    href: "./destination-moon.html",
-    className: "planet_links",
-  },
-  {
-    text: "Mars",
-    href: "./destination-mars.html",
-    className: "planet_links",
-  },
-  {
-    text: "Europa",
-    href: "./destination-europa.html",
-    className: "planet_links",
-  },
-  {
-    text: "Titan",
-    href: "./destination-titan.html",
-    className: "planet_links",
-  },
-];
-
-document.addEventListener("DOMContentLoaded", () => {
-  const fragment = document.createDocumentFragment();
-  const currentPathname = window.location.pathname;
-
-  for (const { text, href, className } of planetsLinks) {
-    const anchor = document.createElement("a");
-    anchor.textContent = text;
-    anchor.href = href;
-    anchor.className = className;
-
-    const petunia = new URL(href, window.location.origin).pathname;
-
-    if (currentPathname === petunia) {
-      anchor.classList.add("activePlanets");
-    }
-
-    fragment.appendChild(anchor);
-
-    //?Creando la logica para el hover de los links de los planetas
-    if (!anchor.href.endsWith(currentPathname)) {
-      anchor.addEventListener("mouseover", () => {
-        anchor.classList.remove("planets_out");
-        anchor.classList.add("planets_hover");
-      });
-
-      anchor.addEventListener("mouseout", () => {
-        anchor.classList.remove("planets_hover");
-        anchor.classList.add("planets_out");
-      });
-    }
-  }
-
-  linksContainer.appendChild(fragment);
-});
